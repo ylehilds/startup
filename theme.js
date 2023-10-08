fa3e069cfb36feb168ee8211fe568927a6f859d3
ylehilds <lehi_alcantara@byu.edu>
2023-10-08 15:57:17 -0600

    finished css styling pages

--- theme.js
+++ theme.js
@@ -1,8 +1,12 @@
 document.getElementById('flexSwitchCheckDefault').addEventListener('change', () => {
     if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
         document.documentElement.setAttribute('data-bs-theme', 'light')
+        document.querySelector('main').style.backgroundColor = 'LightGrey';
     } 
     else {
         document.documentElement.setAttribute('data-bs-theme', 'dark')
+        document.querySelector('main').style.backgroundColor = 'DarkSlateGrey';
     }
 })
+// the below line is to default the theme to dark
+document.querySelector('main').style.backgroundColor = 'DarkSlateGrey';
