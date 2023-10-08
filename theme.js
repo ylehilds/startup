c0710bed15053467688dbe7df64b94297e9a54af
ylehilds <lehi_alcantara@byu.edu>
2023-10-07 18:50:02 -0600

    initial css modifications, still need to finish create and take quiz pages

---
+++ theme.js
@@ -0,0 +1,8 @@
+document.getElementById('flexSwitchCheckDefault').addEventListener('change', () => {
+    if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+        document.documentElement.setAttribute('data-bs-theme', 'light')
+    } 
+    else {
+        document.documentElement.setAttribute('data-bs-theme', 'dark')
+    }
+})
