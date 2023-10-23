64c45e55c5eb2fe569989ec5502b58b0955ec06c
ylehilds <lehi_alcantara@byu.edu>
2023-10-22 18:22:45 -0600

    got login logic working

---
+++ dashboard.js
@@ -0,0 +1,18 @@
+function getUsername() {
+    let users = localStorage.getItem("users")
+    if(users) {
+        users = JSON.parse(users)
+    } else {
+        users = []
+        localStorage.setItem("users", JSON.stringify(users))
+    }
+    let currentUserIndex = users.findIndex(user => user.isLoggedIn == true);
+    if (currentUserIndex == -1) {
+      alert("You are not logged in");
+      return;
+    } else {
+        document.getElementById("username").innerHTML = users[currentUserIndex].username
+    }
+}
+
+getUsername()
---
+++ login.js
@@ -0,0 +1,123 @@
+function signIn() {
+    var usernameAction = document.getElementById("username").value
+    var passwordAction = document.getElementById("password").value
+    
+    let users = localStorage.getItem("users")
+    users = JSON.parse(users)
+    let currentUser = users.find(user => user.username == usernameAction && user.password == passwordAction)
+    if (currentUser != null) {
+        checkLogin(users)
+        let currentUserIndex = users.findIndex(user => user.username == usernameAction && user.password == passwordAction);
+        if (currentUserIndex == -1) {
+        alert("You are not logged in");
+        return;
+        }
+        users[currentUserIndex].isLoggedIn = true;
+        localStorage.setItem("users", JSON.stringify(users))
+        window.location = "dashboard.html"
+    }
+    else {
+        alert("Login failed")
+    }
+}
+
+function signOut(users) {
+    if(!users) {
+        users = localStorage.getItem("users")
+        if(users) {
+            users = JSON.parse(users)
+        } else {
+            users = []
+            localStorage.setItem("users", JSON.stringify(users))
+        }
+    }
+    
+    let currentUserIndex = findCurrentUserIndex(users)
+    if (currentUserIndex == -1) {
+      alert("You are not logged in");
+      return;
+    }
+    users[currentUserIndex].isLoggedIn = false;
+    localStorage.setItem("users", JSON.stringify(users));
+  }
+
+  function signOutRedirect() {
+    signOut()
+    window.location = "index.html"
+  }
+
+function signUp(users) {
+    if(!users) {
+        users = localStorage.getItem("users")
+        if(users) {
+            users = JSON.parse(users)
+        } else {
+            users = []
+            localStorage.setItem("users", JSON.stringify(users))
+        }
+    }
+
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+    if (username && password) {
+        if (isUserAlreadyExist(users, username)) {
+            alert("User already exists, please create a different username!")
+            return
+        }
+        checkLogin(users)
+        const user = { username, password, isLoggedIn: true };
+        users.push(user)
+        localStorage.setItem("users", JSON.stringify(users));
+        window.location = "dashboard.html";
+  } else {
+    alert("SignUp failed");
+  }
+}
+
+function isUserAlreadyExist(users, username) {
+    return users.find(user => user.username == username)
+}
+
+function checkLogin(users) {
+    let currentUser = users.find(user => user.isLoggedIn == true)
+    if (currentUser != null) {
+        signOut(users)
+    }
+}
+
+function pageLoginState() {
+    let users = localStorage.getItem("users")
+    if(users) {
+        users = JSON.parse(users)
+    } else {
+        users = []
+        localStorage.setItem("users", JSON.stringify(users))
+    }
+    let currentUserIndex = findCurrentUserIndex(users)
+    if (currentUserIndex == -1) {
+      console.log("You are not logged in");
+      return;
+    } else {
+        document.getElementById("signIn").style.display = "none"
+        document.getElementById("signUp").style.display = "none"
+        document.getElementById("signOut").style.display = "inline-block"
+        document.getElementById("goToDashboard").style.display = "inline-block"
+        document.getElementById("userPasswordInputs").style.display = "none"
+        }
+}
+
+function goToDashboard() {
+    window.location = "dashboard.html"
+}
+
+function findCurrentUserIndex(users) {
+    return users.findIndex(user => user.isLoggedIn == true);
+}
+
+document.querySelector('#password').addEventListener('keyup', function(event){
+    if (event.keyCode === 13) {
+        signIn()
+    }
+  })
+
+pageLoginState()
