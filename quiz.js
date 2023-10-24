ec8b7c1c68a33de0701a9f60f106fe5c56fe3ddd
ylehilds <lehi_alcantara@byu.edu>
2023-10-23 22:37:53 -0600

    added placeholder for websocket

--- quiz.js
+++ quiz.js
@@ -180,3 +180,10 @@ let questions = [];
         return 'd';
     }
   }
+
+  setInterval(() => {
+    const score = Math.floor(Math.random() * 3000);
+    const chatText = document.querySelector('#players-actions ul');
+    chatText.innerHTML =
+      `<li class="list-group-item"><span class="player-event">Eich</span> scored ${score}</div>` + chatText.innerHTML;
+  }, 5000);
